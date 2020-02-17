e789d975880c66b12a35a576e8537f5dbee35bb8
rcastino <57233470+rcastino@users.noreply.github.com>
2020-02-17 19:54:43 +0100

    UPDte 3

--- Rai71/index.js
+++ Rai71/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Ciao Raimondo2 " + (req.query.name || req.body.name)
+            body: "Ciao Raimondo3 " + (req.query.name || req.body.name)
         };
     }
     else {
